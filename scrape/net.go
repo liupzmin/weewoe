9eb7c6cc00471b4fdc0d1beb0c95139dc771cd65
richard <liupzmin@foxmail.com>
2025-06-11 15:07:25 +0800

    add log

--- scrape/net.go
+++ scrape/net.go
@@ -4,12 +4,15 @@ import (
 	"net"
 	"strconv"
 	"time"
+
+	"github.com/liupzmin/weewoe/log"
 )
 
 func RawConnect(host string, port int64) bool {
 	timeout := time.Second
 	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, strconv.Itoa(int(port))), timeout)
 	if err != nil {
+		log.Errorf("Connect %s:%s Failed: %s", host, port, err)
 		return false
 	}
 
